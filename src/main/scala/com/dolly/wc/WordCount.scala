1a750846dd096e946c22304aaaf9c2b6b3670f18
Senyang Yu <dolly@mail.ustc.edu.cn>
2020-08-01 18:19:23 +0800

    flink批处理wordcount

---
+++ src/main/scala/com/dolly/wc/WordCount.scala
@@ -0,0 +1,31 @@
+package com.dolly.wc
+
+import org.apache.flink.api.scala._
+
+/**
+ *
+ * @author yusenyang 
+ * @create 2020/8/1 18:06
+ */
+
+// 批处理word count程序
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+    // 创建一个执行环境
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    // 从文件中读取数据
+    val inputPath = "/Users/sgcx017/github/flink-tutorial/src/main/resources/hello.txt"
+    val inputDataSet = env.readTextFile(inputPath)
+
+    // 切分数据得到word，然后再按word做分组聚合
+    val wordCountDataSet = inputDataSet
+      .flatMap(_.split(" "))
+      .map((_, 1))
+      .groupBy(0)
+      .sum(1)
+
+    wordCountDataSet.print()
+  }
+}
